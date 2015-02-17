ebccb083cd89af9c2aac7be0ce373bcfc2839db2
Ivy Fae <lalanotlistening@gmail.com>
2015-02-16 22:11:14 -0600

    Started CryptoPartyMN Theme

---
+++ templates/default/CryptoPartyMN/shell/head.tpl.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/banana-brick" type="text/css">
+<link href="<?= \Idno\Core\site()->config()->getDisplayURL() ?>Themes/CryptoPartyMN/css/default.css" rel="stylesheet">
